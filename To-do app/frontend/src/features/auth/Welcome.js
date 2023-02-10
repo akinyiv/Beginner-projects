7f970ae4bc4ed64374010762ab5bf123b5d57cd3
akinyiv <akinyivee10@gmail.com>
2023-02-10 15:47:42 +0300

    working on react

--- To-do app/frontend/src/features/auth/Welcome.js
+++ To-do app/frontend/src/features/auth/Welcome.js
@@ -11,6 +11,7 @@ const Welcome = () => {
     const date = new Date()
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
 
+
     const content = (
         <section className="welcome">
 
